49613277456f8c566bffd22866ea4d5ed11f802c
Ritwik Dutta <ritzymail@gmail.com>
2014-06-08 14:42:05 -0700

    Testing

--- process.py
+++ process.py
@@ -1,13 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-@app.route('/ep')
-def endpoint():
-	return 'Get endpoint.'
+@app.route('/<access_token>')
+def hello_world(access_token):
+    return access_token
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8081)
